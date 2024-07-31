6205fbf12f21b4ba1c86fdbb895a652932bbf1e4
KimChungO <kco796@naver.com>
2024-07-31 12:37:55 +0900

    컴포넌트, props

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const product = "Macbook ";
-const model = "Air";
-const url =
-    "https://file.bodnara.co.kr/logo/insidelogo.php?image=%2Fhttp%3A%2F%2Ffile.bodnara.co.kr%2Fwebedit%2Fnews%2F2010%2F1403651148-mba_12.jpg";
-
-const handleClick = () => {
-    alert("곧 도착합니다.");
-};
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <>
-        <h1>나만의 {product + model} 주문하기</h1>
-        <img src={url} alt="맥북 이미지" />
-        <button onClick={handleClick}>확인</button>
-    </>
-);
+root.render(<App />);
