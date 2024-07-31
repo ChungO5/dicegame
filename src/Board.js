b9ed8d12e68bc8f756ea4c9b7ada7057ad8a9964
KimChungO <kco796@naver.com>
2024-07-31 14:56:58 +0900

    컴포넌트 재사용하기

---
+++ src/Board.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Dice from "./Dice";
+
+const Board = ({ name, color, num, sum, gameHistory }) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <Dice color={color} num={num} />
+            <h2>총점</h2>
+            <p>{sum}</p>
+            <p>{gameHistory.join(", ")}</p>
+        </div>
+    );
+};
+
+export default Board;
