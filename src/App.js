a7d85d5e7c99c724e9b3eb424c8b955ddbbdad11
KimChungO <kco796@naver.com>
2024-07-31 12:50:29 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Dice from "./Dice";
 import Button from "./Button";
 
+const random = (n) => {
+    return Math.ceil(Math.random() * 6);
+};
+
 const App = () => {
+    const [num, setNum] = useState(1);
+
+    const handleRollClick = () => {
+        const nextNum = random(6);
+        setNum(nextNum);
+    };
+
+    const handleClearClick = () => {
+        setNum(1);
+    };
+
     return (
         <div>
             <div>
-                <Button>던지기</Button>
-                <Button>처음부터</Button>
+                <Button onClick={handleRollClick}>던지기</Button>
+                <Button onClick={handleClearClick}>처음부터</Button>
             </div>
-            <Dice color="red" num={2} />
+            <Dice color="red" num={num} />
         </div>
     );
 };
